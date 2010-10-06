0c99e0f2b71a1f57c68f9867e9eb08505b868dae
mhick <mhick@Paros.(none)>
2010-10-06 10:42:24 -0300

    initial check-in

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bivo::Application.config.secret_token = '1dad554ddfd16f29bb4990a77ebe2e345e55cd957c62b2d0876e86a9ff9d4a4983e25bbfa5ba515a31c1dbdbfb228761874c895f2fb68d01c787aadcedf4ccb2'
