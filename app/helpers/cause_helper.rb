f339a39aab450270447096108b822ce01954c7ef
pb <pbrusco@manas.com.ar>
2010-10-07 17:04:44 -0300

    Cause details

---
+++ app/helpers/cause_helper.rb
@@ -0,0 +1,2 @@
+module CauseHelper
+end
---
+++ test/functional/cause_controller_test.rb
@@ -0,0 +1,25 @@
+
+require 'test_helper'
+
+class CauseControllerTest < ActionController::TestCase
+  
+  test "should get details" do
+    cause = Cause.make :url => "foobar"
+    
+    get :details, {'url' => "foobar"} 
+    
+    assert_not_nil assigns(:cause)
+    assert_equal assigns(:cause), cause
+    assert_response :success
+  end
+  
+
+end
+
+
+
+
+#    * :success - Status code was 200
+#    * :redirect - Status code was in the 300-399 range
+#    * :missing - Status code was 404
+#    * :error - Status code was in the 500-599 range
