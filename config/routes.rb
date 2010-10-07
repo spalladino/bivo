69e5ace250cd42a9090515b0916bc0c5775be95e
therma <therma@therma.(none)>
2010-10-07 15:56:27 -0300

    Cause details

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Bivo::Application.routes.draw do
+  get "home/index"
+
+  get "cause/:url", :to => 'cause#details'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -55,4 +59,6 @@ Bivo::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
+  
+  root :to => "home#index" 
 end
