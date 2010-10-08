8ef272007904ad63e77001cf802453dda77bd714
Santiago Palladino <spalladino@manas.com.ar>
2010-10-08 17:22:24 -0300

    Gettext fix

--- config/initializers/fast_gettext.rb
+++ config/initializers/fast_gettext.rb
@@ -1,3 +1,4 @@
 FastGettext.add_text_domain 'app', :path => 'locale', :type => :po
 FastGettext.default_available_locales = ['en','es']
 FastGettext.default_text_domain = 'app'
+FastGettext.default_locale = 'en'
---
+++ locale/model_attributes.rb
@@ -0,0 +1,14 @@
+#DO NOT MODIFY! AUTOMATICALLY GENERATED FILE!
+_('country')
+_('Country|Name')
+_('cause category')
+_('CauseCategory|Name')
+_('cause')
+_('Cause|Name')
+_('Cause|Url')
+_('Cause|Funds needed')
+_('Cause|City')
+_('Cause|Description')
+_('Cause|Funds raised')
+_('Cause|Status')
+#DO NOT MODIFY! AUTOMATICALLY GENERATED FILE!
