fa0a2b24885176c29d2a681635487e64c9a5bef1
pb <pbrusco@manas.com.ar>
2010-10-12 14:59:58 -0300

    Starting cause vote tests

--- app/controllers/cause_controller.rb
+++ app/controllers/cause_controller.rb
@@ -1,9 +1,28 @@
 class CauseController < ApplicationController
   
+  UserId = 1
   
   def details 
     @cause = Cause.find_by_url(params[:url])
   end
   
-  
+  def vote
+    @vote = Vote.new(params[:causeId])
+    if request.xhr?
+      
+      #si es ajax..  json succes:bool message:string
+      # respond to Ajax request
+      
+    else
+      #si no es ajax, recargo la causa y redirect con la url de la causa y meto un flash[:notice] ...
+      if @vote.save
+        render :partial => 'cause'
+      end
+      
+      # respond to normal request
+      
+    end
+  end 
 end
+
+
