20e9e9a73840d51bec25299d11854fd75aa7fa0b
pb <pbrusco@manas.com.ar>
2010-10-12 17:09:17 -0300

    doing vote tests

--- test/functional/cause_controller_test.rb
+++ test/functional/cause_controller_test.rb
@@ -2,9 +2,6 @@ require 'test_helper'
 
 class CauseControllerTest < ActionController::TestCase
   
-  
-  
-  
   test "should get details" do
     cause = Cause.make :url => "foobar"
     
@@ -27,8 +24,6 @@ class CauseControllerTest < ActionController::TestCase
   end
   
   
-  
-  
   test "should get voting causes first page sorted by popularity" do
    (1..100).each { |i| Cause.make_with_votes :votes_count => i, :status => :active }
     
@@ -41,16 +36,38 @@ class CauseControllerTest < ActionController::TestCase
   end
   
   
-  test "should fail on vote if already voted without (ajax)" do
+  test "should fail on vote if already voted (ajax)" do
+    user = create_user
+    user.confirm!
+    sign_in user
+    
+    existing_vote = Vote.find(:first)
+    votes_count = Vote.length
+    
+    xhr :post, :vote, :cause_id => existing_vote.cause_id
+    
+    assert_equal votes_count,Vote.length
+    #deberia testear que se produce un error?
+    
   end
   
+  
   test "should fail on vote if not authenticated user (no ajax)" do
+    cause = Cause.make
+    cause_counter = cause.votes.length
     
+    post :vote, {'cause_id' => cause.id.to_s} 
+    cause.reload
+    
+    assert_equal cause_counter,cause.votes.length
+    #deberia testear que se produce un error?
   end 
+  
   
   test "should fail on vote if cause status doesnt support voting (ajax)" do
     
   end
+  
   
   test "votes should persist in database if everything is ok (ajax)" do
     user = create_user
@@ -62,10 +79,9 @@ class CauseControllerTest < ActionController::TestCase
     
     cause.reload
     
-    assert_not_equal cause.find_by_cause_id_and_user_id(cause.id, user.id), nil
-    
-    
+    assert_not_equal Vote.find_by_cause_id_and_user_id(cause.id, user.id), nil
   end
+  
   
   test "votes should persist in database if everithing is ok (no ajax) " do
     user = create_user
@@ -73,8 +89,12 @@ class CauseControllerTest < ActionController::TestCase
     sign_in user
     cause = Cause.make
     cause_counter = cause.votes.length
-    #TODO: terminar    
+    
+    cause.reload
+    
+    assert_not_equal Vote.find_by_cause_id_and_user_id(cause.id, user.id), nil   
   end
+  
   
   test "votes of the cause should be incremented by one if everthing worked (no ajax)" do
     user = create_user
@@ -87,7 +107,6 @@ class CauseControllerTest < ActionController::TestCase
     cause.reload
     
     assert_equal cause_counter+1,cause.votes.length
-    
   end    
 end
 
