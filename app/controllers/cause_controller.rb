271e21c8517b7c80df5e6a02a7e0ecaa0b6e0c76
pb <pbrusco@manas.com.ar>
2010-10-13 17:09:29 -0300

    voting cause

--- app/controllers/cause_controller.rb
+++ app/controllers/cause_controller.rb
@@ -1,38 +1,40 @@
 class CauseController < ApplicationController
-  
+
   before_filter :authenticate_user!, :except => [ :details, :index ]
-  
-  def details 
+
+  def details
     @cause = Cause.find_by_url(params[:url])
   end
-  
+
   def index
     @causes = Cause.order('votes_count DESC').paginate(:per_page => params[:per_page] || 20, :page => params[:page])
   end
-  
+
   def vote
     @cause = Cause.find_by_id(params[:cause_id])
     @vote = Vote.new(params[:cause_id])
-    if @vote.save 
+    if @vote.save
       custom_response "Ok",true
     else
       custom_response @vote.errors.on(:user_id) ,false
     end
-  end 
- 
-  
-  
-  
+  end
+
+
+
+
   def custom_response(message,success)
-    if request.xhr? 
-      #AJAX 
+    if request.xhr?
+      #AJAX
+      flash[:notice] = message
       render :json =>  {:message => message, :success=> success}
-    else 
+    else
       #NO AJAX
       flash[:notice] = message
       redirect_to request.referer
     end
-  end  
-  
-    
+  end
+
+
 end
+
