b5494b2afbfdbab3cfebfc324247e62c715e538a
Santiago Palladino <spalladino@manas.com.ar>
2010-10-26 11:39:23 -0300

    Initial methods for shop controller

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,10 +1,24 @@
 class ShopsController < ApplicationController
 
+  before_filter :only_admin, :only => [:new, :create, :edit, :update]
+
   def details
     @shop = Shop.find_by_short_url! params[:short_url]
   end
 
+  def new
+  end
+  
+  def edit
+  end
+  
+  def create
+  end
+  
+  def update
+  end
 
+  
 
 end
 
