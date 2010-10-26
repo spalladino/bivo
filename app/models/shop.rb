b1aa05a5184f5fcacc95373bd26c90b1f299f7f7
Santiago Palladino <spalladino@manas.com.ar>
2010-10-26 16:25:48 -0300

    Shop controller tests

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -16,9 +16,9 @@ class Shop < ActiveRecord::Base
   validates_attachment_size :image, :less_than => 1.megabytes
   validates_attachment_content_type :image, :content_type => ['image/jpeg', 'image/png']
 
-  validates_presence_of :name
-  validates_length_of   :name, :maximum => 255
-
+  validates_presence_of   :name
+  validates_length_of     :name, :maximum => 255
+  
   validates_presence_of :description
   validates_length_of   :description, :maximum => 255
   
--- test/functional/shops_controller_test.rb
+++ test/functional/shops_controller_test.rb
@@ -12,7 +12,7 @@ class ShopsControllerTest < ActionController::TestCase
   test "user cannot get new shop" do
     create_and_sign_in
     get :new
-    assert_response :found
+    assert_response :forbidden
   end
   
   test "admin can edit shop" do
@@ -30,19 +30,56 @@ class ShopsControllerTest < ActionController::TestCase
     id = Shop.make.id
     get :edit, :id => id
 
-    assert_response :found
+    assert_response :forbidden
   end
   
   test "should create new shop" do
+    create_admin_and_sign_in
+    assert_difference('Shop.count') do
+      post :create, :shop => {
+        :name => "Shopname",
+        :description => "Testing shop",
+        :short_url => "shop"} 
+    end
+    
+    assert_response :found
   end
   
   test "should not create new shop with no name" do
+    create_admin_and_sign_in
+    post :create, :shop => {
+      :name => "",
+      :description => "Testing shop",
+      :short_url => "shop"} 
+    
+    assert_equal 0, Shop.count
+    assert_response :success
   end
 
   test "should update shop" do
+    create_admin_and_sign_in
+    shop = Shop.make
+
+    post :update, :id => shop.id, :shop => {
+      :name => "Shopname",
+      :description => shop.description,
+      :short_url => shop.short_url } 
+    
+    assert_equal "Shopname", shop.reload.name
+    assert_response :found
   end
   
-  test "should not update shop with repeated name" do
+  test "should not update shop with no description" do
+    create_admin_and_sign_in
+    shop = Shop.make :name => "Shopname"
+
+    post :update, :id => shop.id, :shop => {
+      :name => "",
+      :description => shop.description,
+      :short_url => shop.short_url } 
+    
+    assert_equal "Shopname", shop.reload.name
+    assert_response :success
   end
 
   #DETAILS
