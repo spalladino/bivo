ee60e028595b615361573a9be6f76ff6cf6e128f
Brian J. Cardiff <bcardiff@gmail.com>
2010-11-03 15:04:38 -0300

    basic edition of shop categories. integration pending.

--- app/models/shop_category.rb
+++ app/models/shop_category.rb
@@ -1,4 +1,6 @@
 class ShopCategory < ActiveRecord::Base
+  default_scope :order => 'name'
+  
   acts_as_tree
   has_many :shops, :through => :category_shops
 end
---
+++ test/unit/helpers/shop_categories_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class ShopCategoriesHelperTest < ActionView::TestCase
+end
