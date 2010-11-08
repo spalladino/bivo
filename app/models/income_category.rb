949e0969e3abf3e716c0b09d665985efb84b3b2a
Brian J. Cardiff <bcardiff@gmail.com>
2010-11-08 15:12:14 -0300

    fix singleton income shop category

--- app/models/income_category.rb
+++ app/models/income_category.rb
@@ -7,11 +7,7 @@ class IncomeCategory < ActiveRecord::Base
   ShopName = 'shop'
   
   def self.get_shop_category
-    shop ||= self.find_by_name ShopName
+    @@shop ||= self.find_by_name ShopName
   end
-
-  private
-
-  shop = nil
 
 end
