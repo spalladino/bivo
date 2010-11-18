19f4b7876468616434beebb66c1220f534047397
Brian J. Cardiff <bcardiff@gmail.com>
2010-11-18 18:46:33 -0300

    initial cash pool distribution

--- app/models/accounting/cash_pool_account.rb
+++ app/models/accounting/cash_pool_account.rb
@@ -1,4 +1,19 @@
 class CashPoolAccount < Account
   NAME = 'Cash Pool'
+    
+  def process(movement)
+    # instead of processing flag, maybe a transfer that do not call a initiator of the transaction
+    return if (@processing || false)
+    @processing = true
+    
+    causes = Cause.where(:status => :raising_funds)
+    amount_for_cause = (self.balance / causes.count).to_d
+        
+    causes.each do |cause|
+      Account.transfer self, Account.cause_account(cause), amount_for_cause
+    end
+    
+    @processing = false
+  end
   
 end
--- test/unit/cash_pool_account_test.rb
+++ test/unit/cash_pool_account_test.rb
@@ -10,4 +10,31 @@ class CashPoolAccountTest < ActiveSupport::TestCase
       assert_equal CashPoolAccount::NAME, a.name
     end
   end
+  
+  def make_raising_causes(*causes_spec)
+    result = []
+    causes_spec.each do |spec|
+      result << Cause.make(:status => :active, :funds_needed => 100, :funds_raised => 0)
+      Vote.make :cause => result.last
+      result.last.status = :raising_funds
+      result.last.save!
+    end
+    
+    result
+  end
+  
+  def add_cash_pool_income(amount)    
+    Account.transfer Account.make, Account.cash_pool_account, amount.to_d
+  end
+      
+  test "split funds between raising funds causes" do    
+    c1, c2 = make_raising_causes({}, {})
+    add_cash_pool_income 50
+    
+    c1.reload
+    c2.reload
+    
+    assert_equal 25, c1.funds_raised
+    assert_equal 25, c2.funds_raised
+  end
 end
