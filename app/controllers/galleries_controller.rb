86557020c003f163fa9d1852a86342b4c07b5a0f
pb <pbrusco@manas.com.ar>
2010-11-19 17:37:57 -0300

    gallery test fixed and edit gallery view by entity id

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -1,11 +1,11 @@
 class GalleriesController < ApplicationController
 
   def edit_view
-    @entity = Charity.first
+    @id = params[:entity_id]
+    @gallery_items = GalleryItem.where('gallery_id = ?',Gallery.find_by_entity_id(params[:entity_id])).order('relative_order')
   end
 
   def move_up
-
     gallery_item = GalleryItem.find(params[:id])
     @old_position = gallery_item.relative_order.to_s
     gallery_item.move_up
