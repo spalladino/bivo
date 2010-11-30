25dedfd0256cfda5931dfd6354095bc0e27392cf
Carolina Hadad <carolinahadad@carito.local>
2010-11-30 16:27:21 -0300

    Files added for cucumber tests

---
+++ features/step_definitions/registration_steps.rb
@@ -0,0 +1,13 @@
+When /^I choose charity sign up$/ do
+  Then %(I choose "user_type_charity")
+  sleep 1
+end
+
+When /^I agree terms and conditions$/ do
+  Then %(I check "user_eula_accepted")
+end
+
+Then /^I should be logged in as "([^"]*)"$/ do |mail|
+  Then %(I should see "Signed in as #{mail}")
+end
+
