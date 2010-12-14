eea523fe1d51095da7e40833a2e9b9ef2ea3f728
Brian J. Cardiff <bcardiff@gmail.com>
2010-12-14 16:55:54 -0300

    update causes list. limit active for not admins.

--- test/functional/causes_controller_slow_test.rb
+++ test/functional/causes_controller_slow_test.rb
@@ -4,7 +4,6 @@ class CausesControllerSlowTest < ActionController::TestCase
 
   def self.controller_class; CausesController; end
   
-  
   #LIST-sorted
   test "should get causes list voting status first page sorted by popularity" do
     create_incremental_voted_causes
@@ -45,7 +44,7 @@ class CausesControllerSlowTest < ActionController::TestCase
     assert_equal_unordered causes.map(&:id), assigns(:causes).map(&:id)
   end
 
-#LIST-filter
+  #LIST-filter
   test "should get causes list inactive status filtered by category if admin" do
     user = create_admin_and_sign_in
     CauseCategory.make_many 2
@@ -160,9 +159,9 @@ class CausesControllerSlowTest < ActionController::TestCase
     assert_equal 3, assigns(:causes).size
     assert_causes_returned_order_reverse
   end
-  
+
   #LIST-limit
-  test "should get causes list limiting item count" do
+  test "should get causes list limiting item count if not admin" do
     Cause.make_many 60, :status => :active
 
     get :index, :per_page => 100, :status => :active
@@ -170,7 +169,28 @@ class CausesControllerSlowTest < ActionController::TestCase
     assert_not_nil assigns(:causes)
     assert_equal 50, assigns(:causes).size
   end
-  
+
+  #LIST-limit
+  test "should get causes list without limiting item count if admin" do
+    create_admin_and_sign_in
+    Cause.make_many 60, :status => :active
+
+    get :index, :per_page => 100, :status => :active
+
+    assert_not_nil assigns(:causes)
+    assert_equal 60, assigns(:causes).size
+  end
+
+  #LIST-limit
+  test "should get completed causes list without limiting" do
+    Cause.make_many 60, :status => :completed
+
+    get :index, :per_page => 100, :status => :completed
+
+    assert_not_nil assigns(:causes)
+    assert_equal 60, assigns(:causes).size
+  end
+
   #LIST-filter
   test "should get causes list raising funds filtered by name" do
     causes_ids = [
@@ -193,5 +213,5 @@ class CausesControllerSlowTest < ActionController::TestCase
   def assert_causes_returned_order_reverse(causes=assigns(:causes))
     assert_equal Cause.all.map(&:id).sort.reverse, causes.map(&:id)
   end
-    
+
 end
