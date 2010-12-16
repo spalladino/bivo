4458b5a4c69f32b3209a88226d43e06d4f82f65e
Andrés Taraciuk <ataraciuk@salamis.(none)>
2010-12-16 17:01:39 -0300

    inicio de charity view, incluida la galeria

--- app/models/photo_item.rb
+++ app/models/photo_item.rb
@@ -8,8 +8,8 @@ class PhotoItem < GalleryItem
     true
   end
 
-  has_attached_file :image, :styles => { :thumbnail => "120x90>", :normal => "425x355>", :big_avatar => "150x110>" },
-    :convert_options => { :thumbnail => "-gravity center -extent 120x90", :normal => "-gravity center -extent 425x355", :big_avatar => "-gravity center -extent 150x110" }
+  has_attached_file :image, :styles => { :thumbnail => "69x57>", :normal => "311x183>", :big_avatar => "150x110>" },
+    :convert_options => { :thumbnail => "-gravity center -extent 69x57", :normal => "-gravity center -extent 311x183", :big_avatar => "-gravity center -extent 150x110" }
   
   def thumbnail_url
     self.image.url(:thumbnail)
